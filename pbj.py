16767e4398082a766ca7624fc37a474511424205
Adam Hodges <asdag8@gmail.com>
2011-11-30 22:56:05 -0500

    search stuff

--- pbj.py
+++ pbj.py
@@ -25,7 +25,7 @@ class Client:
         self.upeers = []  # list of connected ultrapeers
         self.upeer = None # ultrapeer of node
         self.searchctr = 0
-        self.completedSearches = []
+        self.completedSearches = {}
 
     def __str__(self):
         if(self.isUltra):
@@ -63,15 +63,15 @@ class Client:
         #requestor is null when requestor=self
         if req.requestor is not None:
             req.ttl = req.ttl-1
-            if (req.searchid in self.completedSearches) or req.ttl<=0:
+
+            if(self.completedSearches[req.requestor] is None):
+                self.completedSearches[req.requestor]=[]
+
+            if (req.searchid in self.completedSearches[req.requestor]) or req.ttl<=0:
                 return
+
+            self.completedSearches[req.requestor].append(req.searchid)
                 
-            ##random way of making sure list doesnt get too long
-            if len(self.completedSearches) > 4:
-                self.completedSearches=[]
-                
-            self.completedSearches.append(req.searchid)
-            
             if self.checkForFile(req.filename) == True:
                 #found file
                 send_found(req.requestor, req.filename)
@@ -80,12 +80,14 @@ class Client:
         if(self.isUltra):
             if self.peers is not None:
                 for p in self.peers:
-                    send_search(req, p)
+                    if p is not req.requestor:
+                        send_search(req, p)
             
             if self.upeers is not None:    
                 for up in self.upeers:
                     #send req to connected upeer
-                    send_search(req, up)
+                    if up is not req.requestor:
+                        send_search(req, up)
         else:
             send_search(req, self.upeer)
 
