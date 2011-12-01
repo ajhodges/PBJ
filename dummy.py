7b5db5af8c221fffa20e47649c915c3eb1f5d0b4
Adam Hodges <asdag8@gmail.com>
2011-11-30 19:47:46 -0500

    patch

--- dummy.py
+++ dummy.py
@@ -11,10 +11,7 @@ client = Client()
 def main():
     client.connectToNetwork()
     
-    server = Process(target=runServer)
-
-def runServer():
-    httpserv.app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 if __name__ == '__main__':
     main()
