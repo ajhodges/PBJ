f61bf1215719c21e26b941461dec4a8c1c41d741
Adam Hodges <asdag8@gmail.com>
2011-11-30 19:41:47 -0500

    adding virtualenv

--- dummy.py
+++ dummy.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python
+# activate virtualenv
+import os
+activate_this = os.path.expanduser("env/bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
+
 from multiprocessing import Process
 from pbj import Client
 
