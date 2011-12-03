5cd1b62f1f76cc7a8ed0c41787321b9d136bd507
Adam Hodges <asdag8@gmail.com>
2011-12-03 16:27:56 -0500

    BLAH

--- gateway.py
+++ gateway.py
@@ -33,87 +33,76 @@ class Network:
     def __init__(self):
         self.upeerCount = -1
         self.upeers = {}
-        self.root = None
         self.outOfOrder = 0
 
     def findUPeer(self):
         for peer in self.upeers.values():
-            print peer.peers
             if(len(peer.peers.keys()) < PEERS_PER_UPEER):
                 if self.outOfOrder == 0:                 
-                    return up
+                    return peer
 
     def linkUPeer(self, up):
      
-        for id,curup in self.upeers:
-            if(math.log(math.abs((up.upeerid - curup.upeerid)), 2) % 2 == 0):
-                print "Linking ultrapeers %d and %d." % (curup.upeerid, up.upeerid)
-                up.upeers[curup.name] = curup
-                curup.upeers[up.name] = up
+        for ids,curup in self.upeers.items():
+            if(up.upeerid - curup.upeerid != 0):
+                if(math.log(math.fabs((up.upeerid - curup.upeerid)), 2) % 2 == 0):
+                    print "Linking ultrapeers %d and %d." % (curup.upeerid, up.upeerid)
+                    up.upeers[curup.name] = curup
+                    curup.upeers[up.name] = up
 
 
   
     def addPeer(self, name):
+        print "ADDING A PEEEEEEEEEEEEEEEEEEEERR"
         result = {}
-        lock.acquire()
-        if self.root == None:
-            self.upeerCount += 1
+        for id,up in self.upeers.items():
+            if send_ping(up.name) == False:
+                print "Lost Ultrapeer ", id
+                del self.upeers[id]
+                self.outOfOrder += 1
+                self.upeerCount -= 1
+
+
+        upeer = self.findUPeer()
+        if upeer is None:
             result['isUltra'] = True
-            result['uPeers'] = None
-            newNode = Node(name, self.upeerCount)
-            self.root = newNode
-            self.upeers[self.upeerCount] = newNode
-            print "Node %s added to network as Ultrapeer %d." % (name, len(self.upeers))
-            lock.release()
-            return result
-        else:    
-            upeer = self.findUPeer(self.root)
-            if upeer is None:
-                result['isUltra'] = True
-                self.upeerCount += 1
-                if  self.upeersOutOfOrder != 0:
-                    for i in range(self.upeerCount):
-                        if self.upeers.has_key(i):
-                            self.upeersOutOfOrder -= 1
-                            newNode = Node(name, i)
-                            self.upeers[i]= newNode
-                            self.linkUPeer(newNode)
-                            result['uPeers'] = newNode.upeers.keys()
-                            print "Node %s added to network as Ultrapeer %d." % (name, len(self.upeers))
-                            lock.release()
-                            return result
-                
+            self.upeerCount += 1
+            if  self.outOfOrder != 0:
+                self.outOfOrder -= 1
+                for i in range(self.upeerCount):
+                    if self.upeers.has_key(i) == False:
+                        print "HERE 1"
+                      
+                        newNode = Node(name, i)
+                        self.upeers[i]= newNode
+                        self.linkUPeer(newNode)
+                        result['uPeers'] = newNode.upeers.keys()
+                        print "Node %s added to network as Ultrapeer %d." % (name, newNode.upeerid)
 
-
+                        return result
             
-                newNode = Node(name, self.upeerCount)
-                self.upeers[self.upeerCount]= newNode
-                self.linkUPeer(newNode)
-                result['uPeers'] = newNode.upeers.keys()
-                print "Node %s added to network as Ultrapeer %d." % (name, len(self.upeers))
-                lock.release()
-                return result
-                
-            else:
-                newNode = Node(name, -1)
-                newNode.upeer = upeer
-                upeer.peers[name] = newNode
-                print upeer.peers
-                result['isUltra'] = False
-                result['uPeer'] = upeer.name
-                print "Node %s added to network under Ultrapeer %s" % (name, upeer.upeerid)
-                lock.release()
-                return result
-    def pingify(self):
-        while True:
-            time.sleep(5)
-            lock.acquire()
-            for id,up in self.upeers.items():
-                if send_ping(up.name) == False:
-                    del self.upeers[id]
-                    self.upeersOutOfOrder += 1
-                    self.upeerCount -= 1
-	    lock.release()	        
+
+
+            print "HERE2"
+            newNode = Node(name, self.upeerCount)
+            self.upeers[self.upeerCount]= newNode
+            self.linkUPeer(newNode)
+            result['uPeers'] = newNode.upeers.keys()
+            print "Node %s added to network as Ultrapeer %d." % (name, newNode.upeerid)
+
+            return result
+            
+        else:
+            newNode = Node(name, -1)
+            newNode.upeer = upeer
+            upeer.peers[name] = newNode
+            result['isUltra'] = False
+            result['uPeer'] = upeer.name
+            print "Node %s added to network under Ultrapeer %s" % (name, upeer.upeerid)
+
+            return result
+    
+
 
 #def main():
 #    pbj = Network()
@@ -136,10 +125,8 @@ def register():
     return pickle.dumps(data)
 
 if __name__ == "__main__":
-    t = threading.Thread(target = pbj.pingify)
-    t.start()
-	
-    app.run(host='0.0.0.0', debug=True)	
+    
+    app.run(host='0.0.0.0', debug=True)    
     
 
 
