fb5ca7f985a05a25038602a45ef0bb76ba1b47dd
Adam Hodges <asdag8@gmail.com>
2011-12-14 12:33:28 -0500

    asdfSDf

--- httpcli.py
+++ httpcli.py
@@ -72,7 +72,7 @@ def send_search(searchreq, superpeerip):
 
 def send_found(port, request):
     '''called by node if the file has been found'''
-    url="http://"+request.requestor+"/result"
+    url="http://"+request.requestor+":"+reqest.requestorport+"/result"
     values={'result':pickle.dumps(request), 'port':port}
     data=urllib.urlencode(values)
     req=urllib2.Request(url,data)
